5085cfd7a5c3114f6399700093176f406c8816a1
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-05 03:54:17 +0700

    refactor(employee): use crudController template instead of employeeController

---
+++ src/controllers/crud-controller.js
@@ -0,0 +1,66 @@
+const createError = require("../utils/create-error");
+
+exports.createRecord = (Model, recordName) => {
+    return async (req, res, next) => {
+        try {
+            // for Employee: check if authenticated user try to create his/her self
+            if (recordName === 'employee' && req.user.id === req.body.userId) { createError("can't create yourself, you are employee", 400) }
+
+            // insert record into table
+            await Model.create(req.body);
+
+            // response with success message
+            res.status(200).json({ message: `${recordName} was successfully created` });
+        } catch (err) {
+            next(err);
+        }
+    };
+};
+
+exports.updateRecord = (Model, recordId, recordName) => {
+    return async (req, res, next) => {
+        try {
+            // update record in table
+            const [totalUpdate] = await Model.update(req.body, { where: { id: +req.params[recordId] } });
+
+            // throw error (invalid record id)
+            if (totalUpdate === 0) { createError(`invalid ${recordName} id`, 400) }
+
+            // response with success message
+            res.status(200).json({ message: `${recordName} was successfully updated` });
+        } catch (err) {
+            next(err);
+        }
+    };
+};
+
+exports.deleteRecord = (Model, recordId, recordName) => {
+    return async (req, res, next) => {
+        try {
+            // delete record from table
+            const totalDelete = await Model.destroy({ where: { id: +req.params[recordId] } });
+
+            // throw error (invalid record id)
+            if (totalDelete === 0) { createError(`invalid ${recordName} id`, 400) }
+
+            // response just success status 204
+            res.status(204).json();
+        } catch (err) {
+            next(err);
+        }
+    };
+};
+
+exports.getAllRecords = (Model) => {
+    return async (req, res, next) => {
+        try {
+            // get all records from table
+            const records = await Model.findAll();
+
+            // response with all records data
+            res.status(200).json({ records });
+        } catch (err) {
+            next(err);
+        }
+    };
+};
--- src/routes/super-user-route.js
+++ src/routes/super-user-route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const employeeController = require('../controllers/employee-controller');
+const { Employee } = require('../models');
+
+const crudController = require('../controllers/crud-controller');
 
 const router = express.Router();
 
-router.post('/employees/:userId', employeeController.createEmployee);   // role = ADMIN by default
-router.patch('/employees/:employeeId', employeeController.updateEmployeeRole);
-router.delete('/employees/:employeeId', employeeController.deleteEmployee);
-router.get('/employees', employeeController.getAllEmployee);
+router.post('/employees', crudController.createRecord(Employee, 'employee'));   // role = ADMIN by default
+router.get('/employees', crudController.getAllRecords(Employee));
+router.patch('/employees/:employeeId', crudController.updateRecord(Employee, 'employeeId', 'employee'));
+router.delete('/employees/:employeeId', crudController.deleteRecord(Employee, 'employeeId', 'employee'));
 
 module.exports = router;
