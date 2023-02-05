a4124043649c7ab90487f0832e29a5dd4631d567
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-05 23:40:47 +0700

    feat(user): add create, update and delete feature for /user/cart

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 // authenticated only !!!
 const { Cart } = require('../models');
+const createError = require('../utils/create-error');
 
 exports.getMyInfo = async (req, res, next) => {
     res.status(200).json({ user: req.user });
@@ -7,7 +8,7 @@ exports.getMyInfo = async (req, res, next) => {
 
 exports.getMyCart = async (req, res, next) => {
     try {
-        // get all products in cart
+        // get all products in Cart table
         const carts = await Cart.findAll({
             where: { userId: req.user.id },
             order: [['updatedAt', 'DESC']]
@@ -15,6 +16,70 @@ exports.getMyCart = async (req, res, next) => {
 
         // response with all items in cart of authenticated user
         res.status(200).json({ carts });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.addMyCart = async (req, res, next) => {
+    try {
+        // check if userId x productId is already exist (updating instead of creating)
+        const isExist = await Cart.findOne({
+            where: {
+                userId: req.user.id,
+                productId: req.body.productId
+            }
+        });
+
+        // is exist ---> update || is not exist ---> create
+        if (isExist) {
+            const newCart = { ...req.body, amount: req.body.amount + isExist.amount };
+            // update product (amount, note) in Cart table
+            const [totalUpdate] = await Cart.update(newCart, { where: { id: isExist.id } });
+
+            // throw error (invalid cart id)
+            if (totalUpdate === 0) { createError(`invalid cart id`, 400) }
+
+            // response with success message
+            res.status(200).json({ message: `cart was successfully updated (try to add existing product)` });
+        } else {
+            // insert product into Cart table
+            await Cart.create({ ...req.body, userId: req.user.id });
+
+            // response with success message
+            res.status(200).json({ message: `product was successfully added to cart` });
+        }
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.updateMyCart = async (req, res, next) => {
+    try {
+        // update product (amount, note) in Cart table
+        const [totalUpdate] = await Cart.update(req.body, { where: { id: +req.params.cartId } });
+
+        // throw error (invalid cart id)
+        if (totalUpdate === 0) { createError(`invalid cart id`, 400) }
+
+        // response with success message
+        res.status(200).json({ message: `cart was successfully updated` });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.deleteMyCart = async (req, res, next) => {
+    try {
+        // delete product from Cart table
+        const totalDelete = await Cart.destroy({ where: { id: +req.params.cartId } });
+
+        // throw error (invalid cart id)
+        if (totalDelete === 0) { createError(`invalid cart id`, 400) }
+
+        // response just success status 204
+        res.status(204).json();
     } catch (err) {
         next(err);
     }
--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -4,7 +4,12 @@ const userController = require('../controllers/user-controller');
 
 const router = express.Router();
 
+// profile
 router.get('/profile', userController.getMyInfo);
+// cart
+router.post('/cart', userController.addMyCart);
 router.get('/cart', userController.getMyCart);
+router.patch('/cart/:cartId', userController.updateMyCart);
+router.delete('/cart/:cartId', userController.deleteMyCart);
 
 module.exports = router;
