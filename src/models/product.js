481f8a31d92e8bad9ea44d60d0a1fd65969b4ff9
PrinceM <parames.pasitprachaja@gmail.com>
2023-02-05 20:59:44 +0700

    fix(model): add unique for product(name)

--- src/models/product.js
+++ src/models/product.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const Product = sequelize.define('Product', {
         name: {
             type: DataTypes.STRING,
+            unique: true,
             allowNull: false,
             validate: { notEmpty: true }
         },
